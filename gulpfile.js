08ad1913dd1be74f3b9f7c62dd4528c8f51e499c
Tim Hemming <timhemming@gmail.com>
2016-01-12 22:22:12 +0000

    Integrating harviewer 1.1 branch. Updated build step for harviewer.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,51 @@
-var gulp = require('gulp');
-var zip = require('gulp-zip');
-var exec = require('child_process').exec;
-var del = require('del');
-var Q = require('q');
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const exec = require('child_process').exec;
+const del = require('del');
 
-gulp.task('package', ['build'], function (done) {
+gulp.task('package', ['build'], done => {
     gulp.src('target/app/**')
         .pipe(zip('harviewer.zip'))
         .pipe(gulp.dest('target'))
-        .on('end', function () {
-            done();
-        });
+        .on('end', done);
 });
 
-gulp.task('build-harviewer', ['clean-harviewer'], function (done) {
-    exec('bash ../../requirejs/build/build.sh app.build.js', {
-        cwd: 'harviewer/webapp/scripts'
-    }, function (err, stdout, stderr) {
+gulp.task('build-harviewer', ['clean-harviewer'], done => {
+    exec('npm i', {
+        cwd: 'harviewer'
+    }, (err, stdout, stderr) => {
         console.error(stderr);
-        done();
+        exec('node ../../node_modules/.bin/r.js -o app.build.js', {
+            cwd: 'harviewer/webapp/scripts'
+        }, (err, stdout, stderr) => {
+            console.error(stderr);
+            done();
+        });
     });
 });
 
 gulp.task('clean', ['clean-harviewer', 'clean-chrome-app']);
 
-gulp.task('clean-harviewer', function (done) {
-    del('harviewer/webapp-build', function () {
-        done();
-    });
+gulp.task('clean-harviewer', done => {
+    del('harviewer/target', done);
 });
 
-gulp.task('clean-chrome-app', function (done) {
-    del(['target'], function () {
-        done();
-    });
+gulp.task('clean-chrome-app', done => {
+    del(['target'], done);
 });
 
-gulp.task('build', ['clean-chrome-app', 'build-harviewer'], function () {
-    var chromeAppDeferred = Q.defer();
-    gulp.src('chrome-app/**')
-        .pipe(gulp.dest('target/app'))
-        .on('end', function () {
-            chromeAppDeferred.resolve();
-        });
+gulp.task('build', ['clean-chrome-app', 'build-harviewer'], () => {
+    const chromeApp = new Promise(resolve => {
+        gulp.src('chrome-app/**')
+            .pipe(gulp.dest('target/app'))
+            .on('end', resolve);
+    });
 
-    var harviewerDeferred = Q.defer();
-    gulp.src('harviewer/webapp-build/**')
-        .pipe(gulp.dest('target/app/harviewer'))
-        .on('end', function () {
-            harviewerDeferred.resolve();
-        });
+    const harviewer = new Promise(resolve => {
+        gulp.src('harviewer/target/**')
+            .pipe(gulp.dest('target/app/harviewer'))
+            .on('end', resolve);
+    });
 
-    return Q.allSettled([
-        chromeAppDeferred.promise,
-        harviewerDeferred.promise
-    ]);
+    return Promise.all([chromeApp, harviewer]);
 });
